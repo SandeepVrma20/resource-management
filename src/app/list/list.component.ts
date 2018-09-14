415b1b689989d24cfd9096ef8ba8799abec65977
Nilesh Singh <nilesh.dharmant.singh@nl.abnamro.com>
2018-09-14 13:56:40 +0200

    Dummy List

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeDetails } from './../employeeDetails';
 
 @Component({
   selector: 'app-list',
@@ -9,9 +10,48 @@ export class ListComponent implements OnInit {
 
   header = 'List';
 
+  displayedColumns: string[] = ['employeeId', 'firstName', 'lastName', 'email', 'phone'];
+  dataSource = ELEMENT_DATA;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+const ELEMENT_DATA: EmployeeDetails[] = [
+  {
+    employeeId: 55914, firstName: 'Rajendra', lastName: 'Kumawat',
+    email: 'kumawat@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 7514, firstName: 'Sandeep', lastName: 'Verma',
+    email: 'sandflash@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 755914, firstName: 'Nilesh', lastName: 'Singh',
+    email: 'singh.nilesh2@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 7554, firstName: 'Ritesh', lastName: 'Agarwal',
+    email: 'ra2@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 75591, firstName: 'Nikhilesh', lastName: 'Singh',
+    email: 'singh.nik@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 7514, firstName: 'Nilesh', lastName: 'Gupta',
+    email: 'nilesh.gupta@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 7554, firstName: 'Fabia', lastName: 'Sharma',
+    email: 'fabil.sharma@tcs.com', phone: '9545825467'
+  },
+  {
+    employeeId: 5914, firstName: 'Carel', lastName: 'Oort',
+    email: 'carel.oort@tcs.com', phone: '9545825467'
+  }
+];
+
